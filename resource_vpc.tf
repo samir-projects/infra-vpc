599a69916293d6d3fe89af418aa89a9d892811c6
samir.sapkota <samir.sapkota@cdw.com>
2025-05-11 23:05:11 -0400

    3rd commit PR

--- resource_vpc.tf
+++ resource_vpc.tf
@@ -1,8 +1,59 @@
+locals {
+    vpc_name = "vpc-${var.username}"
+    public_subnet  = "vpc-${var.username}-publicsubnet"
+    private_subnet = "vpc-${var.username}-privatesubnet"
+    internet_gateway= "igw-${var.username}"
+    route_table = "rt-${var.username}"
+}
 resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block
   instance_tenancy = "default"
 
   tags = {
-    Name = "vpc-main"
+    Name = local.vpc_name
   }
+}
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = local.public_subnet
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = local.private_subnet
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = local.internet_gateway
+  }
+}
+
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = local.route_table
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.route.id
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
 variable "cidr_block" {
-  description = "value of the cidr block"
-  type = string
-  default = "10.0.0.0/16"
+    description = "value of the cidr block"
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "username" {
+    description = "value of the username"
+    type = string
 }
