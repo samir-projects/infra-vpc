793ab297485688b00b6615dd6af3fa5c737fb1b3
samir sapkota <samirsapkota29@gmail.com>
2025-09-05 23:33:51 +0000

    Changes to infra vpc

--- provider.tf
+++ provider.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-s3-state-710413"
-    region = "ca-central-1"
+    bucket         = "terraform-s3-state-710413"
+    region         = "ca-central-1"
+    key            = "terraform/state"
+    dynamodb_table = "terraform-lock-table"
   }
 }
 
--- resource_vpc.tf
+++ resource_vpc.tf
@@ -1,12 +1,13 @@
 locals {
-  vpc_name         = "vpc-${var.username}"
-  public_subnet1   = "vpc-${var.username}-publicsubnet1"
-  public_subnet2   = "vpc-${var.username}-publicsubnet2"
-  private_subnet1  = "vpc-${var.username}-privatesubnet1"
-  private_subnet2  = "vpc-${var.username}-privatesubnet2"
-  internet_gateway = "igw-${var.username}"
-  route_table      = "rt-${var.username}"
-  security_groups  = "SSHSG-${var.username}"
+  vpc_name            = "vpc-${var.username}"
+  public_subnet1      = "vpc-${var.username}-publicsubnet1"
+  public_subnet2      = "vpc-${var.username}-publicsubnet2"
+  private_subnet1     = "vpc-${var.username}-privatesubnet1"
+  private_subnet2     = "vpc-${var.username}-privatesubnet2"
+  internet_gateway    = "igw-${var.username}"
+  private_route_table = "pvt-rt-${var.username}"
+  public_route_table  = "pub-rt-${var.username}"
+  security_groups     = "SSH-SG-${var.username}"
 }
 
 resource "aws_vpc" "main" {
@@ -19,10 +20,11 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public-subnets" {
-  count             = length(var.subnet_cidrs_public)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.subnet_cidrs_public[count.index]
-  availability_zone = var.availability_zones[count.index]
+  count                   = length(var.subnet_cidrs_public)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidrs_public[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private-subnets" {
@@ -40,25 +42,50 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-resource "aws_route_table" "route" {
-  vpc_id = aws_vpc.main.id
+resource "aws_eip" "eip-nat-gateway" {
+  domain = "vpc"
+}
 
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.eip-nat-gateway.id
+  subnet_id     = aws_subnet.public-subnets[1].id
+
+  tags = {
+    Name = "Natgateway"
+  }
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table" "public-route" {
+  vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
-  }
-  tags = {
-    Name = local.route_table
   }
 }
 
 resource "aws_route_table_association" "public_assoc" {
   count          = length(var.subnet_cidrs_public)
-  subnet_id      = element(aws_subnet.public-subnets.*.id, count.index)
-  route_table_id = aws_route_table.route.id
+  subnet_id      = aws_subnet.public-subnets[count.index].id
+  route_table_id = aws_route_table.public-route.id
 }
 
-resource "aws_security_group" "test_sg" {
+resource "aws_route_table" "private-route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
+  }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count          = length(var.subnet_cidrs_private)
+  subnet_id      = aws_subnet.private-subnets[count.index].id
+  route_table_id = aws_route_table.private-route.id
+}
+
+resource "aws_security_group" "ssh-sg" {
   name        = local.security_groups
   vpc_id      = aws_vpc.main.id
   description = "Allow SSH access to the host"
